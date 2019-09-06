1092a4aa64abf0514b0268bcc7fdba408b5d7424
Matthew Lewandowski <matthew.lewandowski@wework.com>
2019-09-06 00:37:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  word.scan(/\A[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/\A+un\z+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\w{5}/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  (/)
 end
 
 def valid_phone_number?(phone)
-
+  phone.grep(a{10}[0-9])
 end
